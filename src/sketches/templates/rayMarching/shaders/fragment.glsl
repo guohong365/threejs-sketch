da999db34fb2f61fe693f27039c54794024938a0
alphardex <2582347430@qq.com>
2022-03-20 12:59:03 +0800

    update

--- src/sketches/templates/rayMarching/shaders/fragment.glsl
+++ src/sketches/templates/rayMarching/shaders/fragment.glsl
@@ -1,151 +1,159 @@
-#pragma glslify:PI=require(glsl-constants/PI)
-#pragma glslify:rotate=require(glsl-rotate)
-#pragma glslify:sdSphere=require(glsl-sdf-primitives/sdSphere)
-#pragma glslify:sdBox=require(glsl-sdf-primitives/sdBox)
-#pragma glslify:sdTorus=require(glsl-sdf-primitives/sdTorus)
-#pragma glslify:sdPlane=require(glsl-sdf-primitives/sdPlane)
-#pragma glslify:sdCappedCylinder=require(glsl-sdf-primitives/sdCappedCylinder)
-#pragma glslify:smin=require(glsl-smooth-min)
-#pragma glslify:opRep=require(glsl-sdf-ops/repeat)
-#pragma glslify:opI=require(glsl-sdf-ops/intersection)
-#pragma glslify:opS=require(glsl-sdf-ops/subtraction)
+#pragma glslify:sdPlane=require(glsl-sdf-primitives-all/sdPlane)
+#pragma glslify:sdSphere=require(glsl-sdf-primitives-all/sdSphere)
+#pragma glslify:sdBox=require(glsl-sdf-primitives-all/sdBox)
+#pragma glslify:sdBoundingBox=require(glsl-sdf-primitives-all/sdBoundingBox)
+#pragma glslify:sdEllipsoid=require(glsl-sdf-primitives-all/sdEllipsoid)
+#pragma glslify:sdTorus=require(glsl-sdf-primitives-all/sdTorus)
+#pragma glslify:sdCappedTorus=require(glsl-sdf-primitives-all/sdCappedTorus)
+#pragma glslify:sdHexPrism=require(glsl-sdf-primitives-all/sdHexPrism)
+#pragma glslify:sdOctogonPrism=require(glsl-sdf-primitives-all/sdOctogonPrism)
+#pragma glslify:sdCapsule=require(glsl-sdf-primitives-all/sdCapsule)
+#pragma glslify:sdRoundCone=require(glsl-sdf-primitives-all/sdRoundCone)
+#pragma glslify:sdTriPrism=require(glsl-sdf-primitives-all/sdTriPrism)
+#pragma glslify:sdCylinder=require(glsl-sdf-primitives-all/sdCylinder)
+#pragma glslify:sdCone=require(glsl-sdf-primitives-all/sdCone)
+#pragma glslify:sdCappedCone=require(glsl-sdf-primitives-all/sdCappedCone)
+#pragma glslify:sdSolidAngle=require(glsl-sdf-primitives-all/sdSolidAngle)
+#pragma glslify:sdOctahedron=require(glsl-sdf-primitives-all/sdOctahedron)
+#pragma glslify:sdPyramid=require(glsl-sdf-primitives-all/sdPyramid)
+#pragma glslify:sdRhombus=require(glsl-sdf-primitives-all/sdRhombus)
+#pragma glslify:iBox=require(glsl-sdf-primitives-all/iBox)
 #pragma glslify:opU=require(glsl-sdf-ops/union)
-#pragma glslify:centerUv=require(glsl-takara/centerUv)
+#pragma glslify:setCamera=require(glsl-takara/setCamera)
 #pragma glslify:getRayDirection=require(glsl-takara/getRayDirection)
-#pragma glslify:sdRoundBox=require(glsl-takara/sdRoundBox)
-#pragma glslify:opTwist=require(glsl-takara/twist)
+#pragma glslify:centerUv=require(glsl-takara/centerUv)
 #pragma glslify:diffuse=require(glsl-takara/diffuse)
 
 uniform float uTime;
-uniform vec2 uMouse;
 uniform vec2 uResolution;
+uniform vec2 uMouse;
 
 varying vec2 vUv;
 
-float sphere(vec3 p){
-    vec3 p1=p;
-    float geo=sdSphere(p1,.6);
-    float result=geo;
-    return result;
+vec2 map(in vec3 pos)
+{
+    vec2 res=vec2(1e10,0.);
+    
+    res=opU(res,vec2(sdSphere(pos-vec3(-2.,.25,0.),.25),26.9));
+    
+    res=opU(res,vec2(sdBoundingBox(pos-vec3(0.,.25,0.),vec3(.3,.25,.2),.025),16.9));
+    res=opU(res,vec2(sdTorus((pos-vec3(0.,.30,1.)).xzy,vec2(.25,.05)),25.));
+    res=opU(res,vec2(sdCone(pos-vec3(0.,.45,-1.),vec2(.6,.8),.45),55.));
+    res=opU(res,vec2(sdCappedCone(pos-vec3(0.,.25,-2.),.25,.25,.1),13.67));
+    res=opU(res,vec2(sdSolidAngle(pos-vec3(0.,0.,-3.),vec2(3,4)/5.,.4),49.13));
+    
+    res=opU(res,vec2(sdCappedTorus((pos-vec3(1.,.30,1.))*vec3(1,-1,1),vec2(.866025,-.5),.25,.05),8.5));
+    res=opU(res,vec2(sdBox(pos-vec3(1.,.25,0.),vec3(.3,.25,.1)),3.));
+    res=opU(res,vec2(sdCapsule(pos-vec3(1.,0.,-1.),vec3(-.1,.1,-.1),vec3(.2,.4,.2),.1),31.9));
+    res=opU(res,vec2(sdCylinder(pos-vec3(1.,.25,-2.),vec2(.15,.25)),8.));
+    res=opU(res,vec2(sdHexPrism(pos-vec3(1.,.2,-3.),vec2(.2,.05)),18.4));
+    
+    res=opU(res,vec2(sdPyramid(pos-vec3(-1.,-.6,-3.),1.),13.56));
+    res=opU(res,vec2(sdOctahedron(pos-vec3(-1.,.15,-2.),.35),23.56));
+    res=opU(res,vec2(sdTriPrism(pos-vec3(-1.,.15,-1.),vec2(.3,.05)),43.5));
+    res=opU(res,vec2(sdEllipsoid(pos-vec3(-1.,.25,0.),vec3(.2,.25,.05)),43.17));
+    res=opU(res,vec2(sdRhombus((pos-vec3(-1.,.34,1.)).xzy,.15,.25,.04,.08),17.));
+    
+    res=opU(res,vec2(sdOctogonPrism(pos-vec3(2.,.2,-3.),.2,.05),51.8));
+    res=opU(res,vec2(sdCylinder(pos-vec3(2.,.15,-2.),vec3(.1,-.1,0.),vec3(-.2,.35,.1),.08),31.2));
+    res=opU(res,vec2(sdCappedCone(pos-vec3(2.,.10,-1.),vec3(.1,0.,0.),vec3(-.2,.40,.1),.15,.05),46.1));
+    res=opU(res,vec2(sdRoundCone(pos-vec3(2.,.15,0.),vec3(.1,0.,0.),vec3(-.1,.35,.1),.15,.05),51.7));
+    res=opU(res,vec2(sdRoundCone(pos-vec3(2.,.20,1.),.2,.1,.3),37.));
+    
+    return res;
 }
 
-float box(vec3 p){
-    vec3 p1=p;
-    // p1=rotate(p1,vec3(1.),uTime);
-    // p1=opRep(p1,vec3(2.5));
-    float geo=sdBox(p1,vec3(.6));
-    float result=geo;
-    return result;
-}
-
-float roundBox(vec3 p){
-    vec3 p1=p;
-    // p1=opRep(p1,vec3(2.5));
-    float geo=sdRoundBox(p1,vec3(.54),.06);
-    float result=geo;
-    return result;
-}
-
-float torusAndPlaneUnion(vec3 p){
-    p=rotate(p,vec3(1.,0.,0.),45.);
-    vec3 p1=p;
-    vec3 p2=p;
-    // p1=rotate(p1,vec3(1.,0.,0.),uTime);
-    p1.y+=.1;
-    p2.y-=.3;
-    float geo1=sdTorus(p1,vec2(.6,.2));
-    float geo2=sdPlane(p2,vec4(vec3(0.,1.,0.),.5));
-    float result=opU(geo1,geo2);
-    return result;
-}
-
-float boxAndTorusSubtraction(vec3 p){
-    p=rotate(p,vec3(1.),uTime);
-    vec3 p1=p;
-    vec3 p2=p;
-    float geo1=sdRoundBox(p1,vec3(1.,.2,.2),.1);
-    float geo2=sdTorus(p1,vec2(.6,.2));
-    float result=opS(geo2,geo1);
-    return result;
-}
-
-float boxAndTorusAndCylinderSmin(vec3 p){
-    p=rotate(p,vec3(1.),uTime);
-    // p=opTwist(p,sin(uTime)*8.);
-    vec3 p1=p;
-    vec3 p2=p;
-    vec3 p3=p;
-    float geo1=sdRoundBox(p1,vec3(1.,.2,.2),.1);
-    float geo2=sdTorus(p1,vec2(.6,.2));
-    float geo3=sdCappedCylinder(p3,vec2(.3,.3));
-    float result=smin(smin(geo2,geo1,.05),geo3,.05);
-    return result;
-}
-
-// main stage
-float sdf(vec3 p){
-    // float result=sphere(p);
-    // float result=box(p);
-    // float result=roundBox(p);
-    float result=torusAndPlaneUnion(p);
-    // float result=boxAndTorusSubtraction(p);
-    // float result=boxAndTorusAndCylinderSmin(p);
-    return result;
-}
-
-vec2 sdfNormal(vec3 p){
-    return vec2(sdf(p),1.);
-}
-
-#pragma glslify:getNormal=require(glsl-sdf-normal,map=sdfNormal)
-#pragma glslify:softshadow=require(glsl-sdf-ops/softshadow,map=sdfNormal)
-
-float rayMarch(vec3 eye,vec3 ray,float end){
-    float depth=0.;
-    for(int i=0;i<256;i++){
-        vec3 pos=eye+depth*ray;
-        float dist=sdf(pos);
-        depth+=dist;
-        if(dist<.0001||dist>=end){
-            break;
+vec2 raycast(in vec3 ro,in vec3 rd)
+{
+    vec2 res=vec2(-1.,-1.);
+    
+    float tmin=1.;
+    float tmax=20.;
+    
+    // raytrace floor plane
+    float tp1=(0.-ro.y)/rd.y;
+    if(tp1>0.)
+    {
+        tmax=min(tmax,tp1);
+        res=vec2(tp1,1.);
+    }
+    
+    // raymarch primitives
+    vec2 tb=iBox(ro-vec3(0.,.4,-.5),rd,vec3(2.5,.41,3.));
+    if(tb.x<tb.y&&tb.y>0.&&tb.x<tmax)
+    {
+        tmin=max(tb.x,tmin);
+        tmax=min(tb.y,tmax);
+        
+        float t=tmin;
+        for(int i=0;i<70&&t<tmax;i++)
+        {
+            vec2 h=map(ro+rd*t);
+            if(abs(h.x)<(.0001*t))
+            {
+                res=vec2(t,h.y);
+                break;
+            }
+            t+=h.x;
         }
     }
-    return depth;
+    
+    return res;
 }
 
-void main(){
+#pragma glslify:calcSoftshadow=require(glsl-sdf-ops/softshadow,map=map)
+#pragma glslify:calcNormal=require(glsl-sdf-normal,map=map)
+#pragma glslify:calcAO=require(glsl-sdf-ops/ao,map=map)
+
+vec3 render(in vec3 ro,in vec3 rd)
+{
+    // background
+    vec3 col=vec3(.7,.7,.9);
+    
+    // raycast scene
+    vec2 res=raycast(ro,rd);
+    float t=res.x;
+    float m=res.y;
+    
+    vec3 pos=ro+t*rd;
+    vec3 nor=(m<1.5)?vec3(0.,1.,0.):calcNormal(pos);
+    
+    // material
+    col=.2+.2*sin(m*2.+vec3(0.,1.,2.));
+    
+    // lighting
+    vec3 lin=vec3(0.);
+    
+    // sun
+    vec3 lig=normalize(vec3(-.5,.4,-.6));
+    float dif=diffuse(lig,nor,2.5);
+    dif*=calcSoftshadow(pos,lig,.02,2.5);
+    lin+=col*2.20*dif*vec3(1.30,1.,.70);
+    
+    col=lin;
+    
+    return col;
+}
+
+void main()
+{
+    // pixel coordinates
     vec2 p=centerUv(vUv,uResolution);
     
-    vec3 ro=vec3(0.,0.,8.);// ray origin
-    vec3 ta=vec3(0.,0.,0.);// look-at target
-    float fl=2.5;// focal length
+    // camera
+    // ray origin
+    vec3 ro=vec3(-4.5,1.3,-4.5);
+    // look-at target
+    vec3 ta=vec3(1.,0.,0.);
+    
+    // focal length
+    const float fl=3.5;
+    
+    // ray direction
     vec3 rd=getRayDirection(p,ro,ta,fl);
     
-    vec3 color=vec3(0.);
+    // render
+    vec3 col=render(ro,rd);
     
-    float end=24.;
-    float depth=rayMarch(ro,rd,end);
-    if(depth<end){
-        vec3 pos=ro+depth*rd;
-        vec3 normal=getNormal(pos);
-        color=normal;
-        
-        // diffuse
-        vec3 lightDir=vec3(-.5,.5,.5);
-        float diff=diffuse(lightDir,normal,2.);
-        
-        // tile
-        float u=1.-floor(mod(pos.x,2.));
-        float v=1.-floor(mod(pos.z,2.));
-        if((u==1.&&v<1.)||(u<1.&&v==1.)){
-            diff*=.7;
-        }
-        
-        // softshadow
-        float shadow=softshadow(pos,lightDir,.02,2.5);
-        diff*=shadow;
-        
-        color=vec3(diff);
-    }
-    gl_FragColor=vec4(color,1.);
+    gl_FragColor=vec4(col,1.);
 }
