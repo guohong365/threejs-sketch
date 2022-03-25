9616aa1461ad67201abfd93f9a6e54e69836d878
alphardex <2582347430@qq.com>
2022-03-25 15:15:27 +0800

    update

--- src/sketches/playground/shaders/fragment.glsl
+++ src/sketches/playground/shaders/fragment.glsl
@@ -1,5 +1,5 @@
 void mainImage(out vec4 fragColor,in vec2 fragCoord){
     vec2 p=fragCoord/iResolution.xy;
-    vec3 color=vec3(p,0.);
-    fragColor=vec4(color,1.);
+    vec3 col=vec3(p,0.);
+    fragColor=vec4(col,1.);
 }
