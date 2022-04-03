9213cb9df003256e317aab149e7c77b36c367cb0
alphardex <2582347430@qq.com>
2022-04-03 13:14:26 +0800

    update

--- src/sketches/templates/image/shaders/main/fragment.glsl
+++ src/sketches/templates/image/shaders/main/fragment.glsl
@@ -7,7 +7,7 @@ uniform sampler2D uTexture;
 varying vec2 vUv;
 
 void main(){
-    vec4 tex=texture2D(uTexture,vUv);
+    vec4 tex=texture(uTexture,vUv);
     vec3 color=tex.rgb;
     gl_FragColor=vec4(color,1.);
 }
