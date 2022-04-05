aadfd6a265b36df6b53bf59e50a242afe6e70d51
alphardex <2582347430@qq.com>
2022-04-05 19:55:18 +0800

    update

--- src/sketches/smash-hit/2/components/shooter.ts
+++ src/sketches/smash-hit/2/components/shooter.ts
@@ -7,24 +7,25 @@ class Shooter extends kokomi.Component {
     super(base);
   }
   addExisting(): void {
-    const { base } = this;
-
     window.addEventListener("click", () => {
-      const ball = new Ball(base);
-      ball.addExisting();
-
-      // 追踪鼠标位置
-      const p = new THREE.Vector3(0, 0, 0);
-      p.copy(this.base.interactionManager.raycaster.ray.direction);
-      p.add(this.base.interactionManager.raycaster.ray.origin);
-      ball.body.position.set(p.x, p.y, p.z);
-
-      // 给予鼠标方向的速度
-      const v = new THREE.Vector3(0, 0, 0);
-      v.copy(this.base.interactionManager.raycaster.ray.direction);
-      v.multiplyScalar(24);
-      ball.body.velocity.set(v.x, v.y, v.z);
+      this.shootBall();
     });
+  }
+  shootBall() {
+    const ball = new Ball(this.base);
+    ball.addExisting();
+
+    // 追踪鼠标位置
+    const p = new THREE.Vector3(0, 0, 0);
+    p.copy(this.base.interactionManager.raycaster.ray.direction);
+    p.add(this.base.interactionManager.raycaster.ray.origin);
+    ball.body.position.set(p.x, p.y, p.z);
+
+    // 给予鼠标方向的速度
+    const v = new THREE.Vector3(0, 0, 0);
+    v.copy(this.base.interactionManager.raycaster.ray.direction);
+    v.multiplyScalar(24);
+    ball.body.velocity.set(v.x, v.y, v.z);
   }
 }
 
