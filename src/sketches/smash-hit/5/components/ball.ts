3ba42de4672f3b77fea7ba21974a027de60191ba
alphardex <2582347430@qq.com>
2022-04-06 16:35:28 +0800

    update

---
+++ src/sketches/smash-hit/5/components/ball.ts
@@ -0,0 +1,33 @@
+import * as THREE from "three";
+import * as kokomi from "kokomi.js";
+import * as CANNON from "cannon-es";
+
+class Ball extends kokomi.Component {
+  mesh: THREE.Mesh;
+  body: CANNON.Body;
+  constructor(base: kokomi.Base) {
+    super(base);
+
+    const geometry = new THREE.SphereGeometry(0.25, 64, 64);
+    const material = new kokomi.GlassMaterial({});
+    const mesh = new THREE.Mesh(geometry, material);
+    this.mesh = mesh;
+
+    const shape = kokomi.convertGeometryToShape(geometry);
+    const body = new CANNON.Body({
+      mass: 1,
+      shape,
+      position: new CANNON.Vec3(0, 1, 2),
+    });
+    this.body = body;
+  }
+  addExisting(): void {
+    const { base, mesh, body } = this;
+    const { scene, physics } = base;
+
+    scene.add(mesh);
+    physics.add({ mesh, body });
+  }
+}
+
+export default Ball;
---
+++ src/sketches/smash-hit/5/index.ts
@@ -0,0 +1,69 @@
+import * as kokomi from "kokomi.js";
+import * as THREE from "three";
+import Floor from "./components/floor";
+import Environment from "./components/environment";
+import Shooter from "./components/shooter";
+import Breaker from "./components/breaker";
+import resourceList from "./resources";
+import type Ball from "./components/ball";
+import Game from "./components/game";
+
+class Sketch extends kokomi.Base {
+  create() {
+    const assetManager = new kokomi.AssetManager(this, resourceList);
+    assetManager.emitter.on("ready", () => {
+      const listener = new THREE.AudioListener();
+      this.camera.add(listener);
+
+      const glassBreakAudio = new THREE.Audio(listener);
+      glassBreakAudio.setBuffer(assetManager.items.glassBreakAudio);
+
+      this.camera.position.set(0, 1, 6);
+
+      const environment = new Environment(this);
+      environment.addExisting();
+
+      const floor = new Floor(this);
+      floor.addExisting();
+
+      const shooter = new Shooter(this);
+      shooter.addExisting();
+
+      const breaker = new Breaker(this);
+
+      const game = new Game(this);
+
+      game.emitter.on("create", (obj: any) => {
+        breaker.add(obj);
+      });
+
+      game.createBreakablesByInterval();
+
+      // 当弹珠发射时监听碰撞，如果触发碰撞则粉碎撞到的物体
+      shooter.emitter.on("shoot", (ball: Ball) => {
+        ball.body.addEventListener("collide", (e: any) => {
+          breaker.onCollide(e);
+        });
+      });
+
+      // 弹珠击中物体时
+      breaker.emitter.on("hit", () => {
+        game.incScore();
+        document.querySelector(".score")!.textContent = `${game.score}`;
+        glassBreakAudio.play();
+      });
+
+      this.update(() => {
+        game.moveBreakables(breaker.objs);
+      });
+    });
+  }
+}
+
+const createSketch = () => {
+  const sketch = new Sketch();
+  sketch.create();
+  return sketch;
+};
+
+export default createSketch;
