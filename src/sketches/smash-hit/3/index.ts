d119f1f32b79eb30bbcaad16f90e498dd978316b
alphardex <2582347430@qq.com>
2022-04-06 14:55:38 +0800

    update

--- src/sketches/smash-hit/3/index.ts
+++ src/sketches/smash-hit/3/index.ts
@@ -1,44 +1,56 @@
 import * as kokomi from "kokomi.js";
+import * as THREE from "three";
 import Floor from "./components/floor";
 import Environment from "./components/environment";
 import Box from "./components/box";
 import Ball from "./components/ball";
 import Shooter from "./components/shooter";
 import Breaker from "./components/breaker";
+import resourceList from "./resources";
 
 class Sketch extends kokomi.Base {
   create() {
-    new kokomi.OrbitControls(this);
+    const assetManager = new kokomi.AssetManager(this, resourceList);
+    assetManager.emitter.on("ready", () => {
+      const listener = new THREE.AudioListener();
+      this.camera.add(listener);
 
-    this.camera.position.set(-3, 3, 4);
+      const glassBreakAudio = new THREE.Audio(listener);
+      glassBreakAudio.setBuffer(assetManager.items.glassBreakAudio);
 
-    const environment = new Environment(this);
-    environment.addExisting();
+      new kokomi.OrbitControls(this);
 
-    const floor = new Floor(this);
-    floor.addExisting();
+      this.camera.position.set(-3, 3, 4);
 
-    const box = new Box(this);
-    box.addExisting();
+      const environment = new Environment(this);
+      environment.addExisting();
 
-    const ball = new Ball(this);
-    ball.addExisting();
+      const floor = new Floor(this);
+      floor.addExisting();
 
-    const shooter = new Shooter(this);
-    shooter.addExisting();
+      const box = new Box(this);
+      box.addExisting();
 
-    const breaker = new Breaker(this);
+      const ball = new Ball(this);
+      ball.addExisting();
 
-    // 定义好所有可粉碎的物体
-    const breakables = [box];
-    breakables.forEach((item) => {
-      breaker.add(item);
-    });
+      const shooter = new Shooter(this);
+      shooter.addExisting();
 
-    // 当弹珠发射时监听碰撞，如果触发碰撞则粉碎撞到的物体
-    shooter.emitter.on("shoot", (ball: Ball) => {
-      ball.body.addEventListener("collide", (e: any) => {
-        breaker.onCollide(e);
+      const breaker = new Breaker(this);
+
+      // 定义好所有可粉碎的物体
+      const breakables = [box];
+      breakables.forEach((item) => {
+        breaker.add(item);
+      });
+
+      // 当弹珠发射时监听碰撞，如果触发碰撞则粉碎撞到的物体
+      shooter.emitter.on("shoot", (ball: Ball) => {
+        ball.body.addEventListener("collide", (e: any) => {
+          glassBreakAudio.play();
+          breaker.onCollide(e);
+        });
       });
     });
   }
