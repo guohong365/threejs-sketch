2c2ca84fb0fc226605b92c7332e2f3d5e9d51a22
alphardex <2582347430@qq.com>
2022-04-08 14:53:07 +0800

    update

---
+++ src/components/PanoramaScene.vue
@@ -0,0 +1,142 @@
+<script lang="ts" setup>
+import createSketch from "@/sketches/panorama/fromConfig";
+import dummyConfig from "@/sketches/panorama/fromConfig/config";
+
+import { nextTick, onMounted, reactive } from "vue";
+
+import type * as THREE from "three";
+import type * as kokomi from "kokomi.js";
+
+interface State {
+  panoramaConfig: kokomi.PanoramaConfig;
+  infospotsConfig: kokomi.InfospotConfig[];
+  mode: "view" | "edit";
+}
+
+const state: State = reactive({
+  panoramaConfig: [],
+  infospotsConfig: [],
+  mode: "view",
+});
+
+let sketch: ReturnType<typeof createSketch> | null = null;
+
+// 初始化
+const init = () => {
+  return new Promise((resolve) => {
+    sketch = createSketch();
+
+    const generator = sketch.generator;
+    generator.generateByConfig(state.panoramaConfig);
+    generator.emitter.on("generate", (generator: kokomi.PanoramaGenerator) => {
+      state.infospotsConfig = generator.allInfospotConfig;
+      nextTick(() => {
+        generator.generateInfospotsWithSceneJump();
+        resolve(true);
+      });
+    });
+  });
+};
+
+// 开启编辑点功能
+const enablePointEdit = () => {
+  const generator = sketch?.generator;
+  if (!generator) {
+    return;
+  }
+
+  generator.outputCurrentScenePosition();
+  generator.emitter.on("click-scene", (point: THREE.Vector3) => {
+    if (state.mode !== "edit") {
+      return;
+    }
+
+    addPoint(point);
+  });
+};
+
+// 获取当前场景配置
+const getCurrentSceneConfig = () => {
+  const generator = sketch?.generator;
+  if (!generator) {
+    return null;
+  }
+
+  const sceneConfig = state.panoramaConfig.find(
+    (scene: kokomi.SceneConfig) =>
+      scene.id === generator.viewer?.currentPanorama?.id
+  );
+  return sceneConfig;
+};
+
+// 添加点
+const addPoint = (point: THREE.Vector3) => {
+  const generator = sketch?.generator;
+  if (!generator) {
+    return;
+  }
+
+  const currentSceneConfig = getCurrentSceneConfig();
+  const infospot = {
+    id: `${generator.allInfospotConfig.length}`,
+    point,
+    name: `${generator.allInfospotConfig.length}`,
+  };
+  currentSceneConfig?.infospots?.push(infospot);
+  sketch?.generator.setConfig(state.panoramaConfig);
+  state.infospotsConfig = generator.allInfospotConfig;
+  nextTick(() => {
+    sketch?.generator.generateInfospotsWithSceneJump();
+  });
+};
+
+onMounted(async () => {
+  state.panoramaConfig = dummyConfig;
+  await init();
+  enablePointEdit();
+});
+</script>
+
+<template>
+  <div id="sketch" class="bg-black w-screen h-screen overflow-hidden"></div>
+  <div class="absolute cover overflow-hidden pointer-events-none">
+    <div class="pointer-events-auto">
+      <template v-for="(item, i) in state.infospotsConfig" :key="i">
+        <div class="point" :class="`point-${item.id}`">
+          <div class="label">{{ item.name }}</div>
+        </div>
+      </template>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.point {
+  position: absolute;
+  top: 0;
+  left: 0;
+  opacity: 0;
+  transform: translate(var(--x), var(--y));
+  z-index: var(--z-index);
+
+  &.visible {
+    opacity: 1;
+    cursor: pointer;
+  }
+
+  .label {
+    position: relative;
+    width: 40px;
+    height: 40px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: #ffffff;
+    text-align: center;
+    font-size: 14px;
+    border-radius: 50%;
+    background: #00000077;
+    border: 1px solid #ffffff77;
+  }
+}
+</style>
