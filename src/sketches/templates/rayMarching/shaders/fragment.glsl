0e4718cef607d56ebc024c4ae633b429f168252c
alphardex <2582347430@qq.com>
2022-04-10 19:17:09 +0800

    update

--- src/sketches/templates/rayMarching/shaders/fragment.glsl
+++ src/sketches/templates/rayMarching/shaders/fragment.glsl
@@ -124,7 +124,19 @@ vec3 getSceneColor(vec2 fragCoord){
 }
 
 void mainImage(out vec4 fragColor,in vec2 fragCoord){
-    vec3 col=getSceneColor(fragCoord);
+    vec3 tot=vec3(0.);
     
-    fragColor=vec4(col,1.);
+    float AA_size=2.;
+    float count=0.;
+    for(float aaY=0.;aaY<AA_size;aaY++)
+    {
+        for(float aaX=0.;aaX<AA_size;aaX++)
+        {
+            tot+=getSceneColor(fragCoord+vec2(aaX,aaY)/AA_size);
+            count+=1.;
+        }
+    }
+    tot/=count;
+    
+    fragColor=vec4(tot,1.);
 }
