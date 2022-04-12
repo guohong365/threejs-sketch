d19647ea6abdfad62d6226ea2bdab9646cae7c6a
alphardex <2582347430@qq.com>
2022-04-12 08:25:28 +0800

    update

--- src/sketches/ray-marching-js/basic/index.ts
+++ src/sketches/ray-marching-js/basic/index.ts
@@ -10,19 +10,19 @@ class Sketch extends kokomi.Base {
 
     const sdfMapFunction = new marcher.SDFMapFunction();
 
-    const sdfLayer = new marcher.SDFLayer();
+    const sdfLayer1 = new marcher.SDFLayer();
 
     const boxSDF = new marcher.BoxSDF({ sdfVarName: "d1" });
     boxSDF.round(0.1);
-    sdfLayer.addPrimitive(boxSDF);
+    sdfLayer1.addPrimitive(boxSDF);
 
     const sphereSDF = new marcher.SphereSDF({ sdfVarName: "d2" });
     sphereSDF.translate(0, -0.75, 0);
-    sdfLayer.addPrimitive(sphereSDF);
+    sdfLayer1.addPrimitive(sphereSDF);
 
     sphereSDF.smoothUnion(boxSDF);
 
-    sdfMapFunction.addLayer(sdfLayer);
+    sdfMapFunction.addLayer(sdfLayer1);
 
     mar.setMapFunction(sdfMapFunction);
 
