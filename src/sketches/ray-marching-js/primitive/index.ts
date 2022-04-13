e53cfde835a155c3e54dcf03e6be1ff592d05ab3
alphardex <2582347430@qq.com>
2022-04-13 11:28:56 +0800

    update

--- src/sketches/ray-marching-js/primitive/index.ts
+++ src/sketches/ray-marching-js/primitive/index.ts
@@ -68,6 +68,8 @@ class Sketch extends kokomi.Base {
 
     mar.setMapFunction(map);
 
+    console.log(map.shader);
+
     const rayMarchingQuad = new kokomi.RayMarchingQuad(this, mar);
     rayMarchingQuad.render();
   }
