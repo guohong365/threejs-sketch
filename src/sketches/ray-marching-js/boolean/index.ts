88e679eccefd7a9d49719fe708759deb587176ab
alphardex <2582347430@qq.com>
2022-04-13 13:27:59 +0800

    update

--- src/sketches/ray-marching-js/boolean/index.ts
+++ src/sketches/ray-marching-js/boolean/index.ts
@@ -12,15 +12,131 @@ class Sketch extends kokomi.Base {
 
     const map = new marcher.SDFMapFunction();
 
-    const layer1 = new marcher.SDFLayer();
+    // union
+    {
+      const layer = new marcher.SDFLayer();
 
-    const box1 = new marcher.BoxSDF({
-      sdfVarName: "d1",
-    });
-    box1.round(0.1);
-    layer1.addPrimitive(box1);
+      const box = new marcher.BoxSDF({
+        sdfVarName: "d1",
+      });
+      box.round(0.1);
+      box.translate(3, 0, 2);
+      layer.addPrimitive(box);
+      const sphere = new marcher.SphereSDF({
+        sdfVarName: "d2",
+      });
+      sphere.translate(3, -0.75, 2);
+      layer.addPrimitive(sphere);
+      box.hide();
+      sphere.union(box);
 
-    map.addLayer(layer1);
+      map.addLayer(layer);
+    }
+
+    // intersection
+    {
+      const layer = new marcher.SDFLayer();
+
+      const box = new marcher.BoxSDF({
+        sdfVarName: "d1",
+      });
+      box.round(0.1);
+      box.translate(0, 0, 2);
+      layer.addPrimitive(box);
+      const sphere = new marcher.SphereSDF({
+        sdfVarName: "d2",
+      });
+      sphere.translate(0, -0.75, 2);
+      layer.addPrimitive(sphere);
+      box.hide();
+      sphere.intersect(box);
+
+      map.addLayer(layer);
+    }
+
+    // subtraction
+    {
+      const layer = new marcher.SDFLayer();
+
+      const box = new marcher.BoxSDF({
+        sdfVarName: "d1",
+      });
+      box.round(0.1);
+      box.translate(-3, 0, 2);
+      layer.addPrimitive(box);
+      const sphere = new marcher.SphereSDF({
+        sdfVarName: "d2",
+      });
+      sphere.translate(-3, -0.75, 2);
+      layer.addPrimitive(sphere);
+      box.hide();
+      sphere.subtract(box);
+
+      map.addLayer(layer);
+    }
+
+    // smooth union
+    {
+      const layer = new marcher.SDFLayer();
+
+      const box = new marcher.BoxSDF({
+        sdfVarName: "d1",
+      });
+      box.round(0.1);
+      box.translate(3, 0, -2);
+      layer.addPrimitive(box);
+      const sphere = new marcher.SphereSDF({
+        sdfVarName: "d2",
+      });
+      sphere.translate(3, -0.75, -2);
+      layer.addPrimitive(sphere);
+      box.hide();
+      sphere.smoothUnion(box);
+
+      map.addLayer(layer);
+    }
+
+    // smooth intersection
+    {
+      const layer = new marcher.SDFLayer();
+
+      const box = new marcher.BoxSDF({
+        sdfVarName: "d1",
+      });
+      box.round(0.1);
+      box.translate(0, 0, -2);
+      layer.addPrimitive(box);
+      const sphere = new marcher.SphereSDF({
+        sdfVarName: "d2",
+      });
+      sphere.translate(0, -0.75, -2);
+      layer.addPrimitive(sphere);
+      box.hide();
+      sphere.smoothIntersect(box);
+
+      map.addLayer(layer);
+    }
+
+    // smooth subtraction
+    {
+      const layer = new marcher.SDFLayer();
+
+      const box = new marcher.BoxSDF({
+        sdfVarName: "d1",
+      });
+      box.round(0.1);
+      box.translate(-3, 0, -2);
+      layer.addPrimitive(box);
+      const sphere = new marcher.SphereSDF({
+        sdfVarName: "d2",
+      });
+      sphere.translate(-3, -0.75, -2);
+      layer.addPrimitive(sphere);
+      box.hide();
+      sphere.smoothSubtract(box);
+
+      map.addLayer(layer);
+    }
 
     mar.setMapFunction(map);
 
