1ed1aa6a384f9cc7f72a1d54fca3b5455c43afa3
alphardex <2582347430@qq.com>
2022-04-18 13:56:33 +0800

    update

--- src/sketches/ray-marching-js/bezier/index.ts
+++ src/sketches/ray-marching-js/bezier/index.ts
@@ -2,6 +2,8 @@ import * as marcher from "marcher.js";
 
 import * as kokomi from "kokomi.js";
 
+import * as dat from "lil-gui";
+
 class Sketch extends kokomi.Base {
   create() {
     new kokomi.OrbitControls(this);
@@ -16,17 +18,23 @@ class Sketch extends kokomi.Base {
 
     const map = new marcher.SDFMapFunction();
 
-    {
-      const layer = new marcher.SDFLayer();
+    const params = {
+      x2: 0,
+      xMax: 1.3,
+      yMax: 0.9,
+      zMax: 0.1,
+    };
 
-      const bezier = new marcher.BezierSDF({
-        sdfVarName: "d1",
-      });
-      layer.addPrimitive(bezier);
-      bezier.round(0.1);
+    const layer = new marcher.SDFLayer();
 
-      map.addLayer(layer);
-    }
+    const bezier = new marcher.BezierSDF({
+      sdfVarName: "d1",
+      ...params,
+    });
+    layer.addPrimitive(bezier);
+    bezier.round(0.1);
+
+    map.addLayer(layer);
 
     mar.setMapFunction(map);
 
@@ -34,6 +42,51 @@ class Sketch extends kokomi.Base {
     rayMarchingQuad.render();
 
     console.log(mar.fragmentShader);
+
+    const gui = new dat.GUI();
+    gui
+      .add(params, "x2")
+      .min(0)
+      .max(1)
+      .step(0.01)
+      .name("Top Vertex")
+      .onChange((value: number) => {
+        bezier.x2 = value;
+        rayMarchingQuad.render();
+      });
+
+    const size = gui.addFolder("Size");
+
+    size
+      .add(params, "xMax")
+      .min(0)
+      .max(5)
+      .step(0.01)
+      .name("x")
+      .onChange((value: number) => {
+        bezier.xMax = value;
+        rayMarchingQuad.render();
+      });
+    size
+      .add(params, "yMax")
+      .min(0)
+      .max(5)
+      .step(0.01)
+      .name("y")
+      .onChange((value: number) => {
+        bezier.yMax = value;
+        rayMarchingQuad.render();
+      });
+    size
+      .add(params, "zMax")
+      .min(0)
+      .max(2)
+      .step(0.01)
+      .name("z")
+      .onChange((value: number) => {
+        bezier.zMax = value;
+        rayMarchingQuad.render();
+      });
   }
 }
 
