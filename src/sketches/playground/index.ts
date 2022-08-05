42c0f16afdddc4545d93a5b50279e3940099b63a
alphardex <2582347430@qq.com>
2022-08-05 10:41:19 +0800

    update

--- src/sketches/playground/index.ts
+++ src/sketches/playground/index.ts
@@ -1,15 +1,29 @@
 import * as kokomi from "kokomi.js";
 
+import * as THREE from "three";
+
+import vertexShader from "./shaders/vertex.glsl";
 import fragmentShader from "./shaders/fragment.glsl";
 
 class Sketch extends kokomi.Base {
   create() {
-    const screenQuad = new kokomi.ScreenQuad(this, {
-      shadertoyMode: true,
+    new kokomi.OrbitControls(this);
+
+    this.camera.position.z = 3;
+
+    const cm = new kokomi.CustomMesh(this, {
+      geometry: new kokomi.HyperbolicHelicoidGeometry(64, 64),
+      vertexShader,
       fragmentShader,
-      uniforms: {},
     });
-    screenQuad.addExisting();
+    cm.material.side = THREE.DoubleSide;
+    cm.addExisting();
+
+    const ambiLight = new THREE.AmbientLight(0xffffff, 0.4);
+    this.scene.add(ambiLight);
+    const dirLight = new THREE.DirectionalLight(0xffffff, 1.2);
+    dirLight.position.set(1, 2, 3);
+    this.scene.add(dirLight);
   }
 }
 
