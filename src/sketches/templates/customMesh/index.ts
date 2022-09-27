0eb1bb0bedc249716f0f4c93b9db0b0acd16d83b
alphardex <2582347430@qq.com>
2022-09-27 08:28:00 +0800

    update

--- src/sketches/templates/customMesh/index.ts
+++ src/sketches/templates/customMesh/index.ts
@@ -7,23 +7,20 @@ import fragmentShader from "./shaders/fragment.glsl";
 
 class Sketch extends kokomi.Base {
   create() {
-    this.camera.position.set(1, 1, 1);
+    this.camera.position.set(0, 0, 2);
 
     new kokomi.OrbitControls(this);
 
     const cm = new kokomi.CustomMesh(this, {
+      baseMaterial: new THREE.ShaderMaterial(),
       geometry: new THREE.PlaneGeometry(1, 1, 16, 16),
       vertexShader,
       fragmentShader,
+      materialParams: {
+        side: THREE.DoubleSide,
+      },
     });
-    cm.mesh.rotation.x = THREE.MathUtils.degToRad(-90);
     cm.addExisting();
-
-    const ambiLight = new THREE.AmbientLight(0xffffff, 0.4);
-    this.scene.add(ambiLight);
-    const dirLight = new THREE.DirectionalLight(0xffffff, 1.2);
-    dirLight.position.set(1, 2, 3);
-    this.scene.add(dirLight);
   }
 }
 
