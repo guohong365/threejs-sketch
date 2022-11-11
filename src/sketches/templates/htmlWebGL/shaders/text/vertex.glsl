4fad4ff3e9012360920c78122280399d0966c302
alphardex <2582347430@qq.com>
2022-11-11 16:21:20 +0800

    update

--- src/sketches/templates/htmlWebGL/shaders/text/vertex.glsl
+++ src/sketches/templates/htmlWebGL/shaders/text/vertex.glsl
@@ -1,11 +1,19 @@
+#pragma glslify:cnoise=require(glsl-noise/classic/3d)
+
 uniform float iTime;
 uniform vec2 iResolution;
 uniform vec2 iMouse;
 
 varying vec2 vUv;
 
+vec3 distort(vec3 p){
+    p.xy+=cnoise(vec3(vec2(p.xy),iTime))*3.;
+    return p;
+}
+
 void main(){
     vec3 p=position;
+    p=distort(p);
     gl_Position=projectionMatrix*modelViewMatrix*vec4(p,1.);
     
     vUv=uv;
